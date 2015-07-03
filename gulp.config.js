1dd7b9804a8e91e09ceb98ac3ee7891ddf47c795
Arturo <arturo.romerotcs@gmail.com>
2015-07-03 08:55:05 -0600

    add more logic to manage Bower Components

--- gulp.config.js
+++ gulp.config.js
@@ -1,20 +1,53 @@
 module.exports = function() {
-  var clients = './src/client/';
+  var client = './src/client/';
+  var clientApp = client + 'app/';
+  var server = './src/server/';
+  var temp = './.tmp/';
 
   var config = {
-
-    temp: './.tmp',
-
-    /*
-    * All file parths
+    /**
+    * Files paths
     */
-    //all JS that I want to vet
     alljs: [
-      './src/**/*.js',
-      './*.js'
+    './src/**/*.js',
+    './*.js'
     ],
+    client: client,
+    css: temp + 'styles.css',
+    index: client + 'index.html',
+    js: [
+    clientApp + '**/*.module.js',
+    clientApp + '**/*.js',
+    '!' + clientApp + '**/*.spec.js'
+    ],
+    less: client + 'styles/styles.less',
+    server: server,
+    temp: temp,
 
-    less: clients + 'styles/styles.less'
+    /**
+    * Bower and NPM locations
+    */
+    bower: {
+      json: require('./bower.json'),
+      directory: './bower_components/',
+      ignorePath: '../..'
+    },
+
+    /**
+    * Node settings
+    */
+    defaultPort: 7203,
+    nodeServer: './src/server/app.js'
+
+  };
+
+  config.getWiredepDefaultOptions = function() {
+    var options = {
+      bowerJson: config.bower.json,
+      directory: config.bower.directory,
+      ignorePath: config.bower.ignorePath
+    };
+    return options;
   };
 
   return config;
